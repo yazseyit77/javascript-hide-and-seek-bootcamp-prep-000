9cc1966948d309378997f11113ac1e0eff75fa52
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-07-26 04:28:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+ var main = document.querySelector("#nested .target");
+ return main;
+}
+
+function  deepestChild(){
+  var deep = document.querySelector("#grand-node div div div div");
+  return deep;
+}
+
+function increaseRankBy(n){
+  var n = document.querySelectorAll(".ranked-list");
+  for(var i = 0; i < n.length; i++){
+    n[i].innerHTML = (i + 1).toString();
+  }
+  return n;
+}
